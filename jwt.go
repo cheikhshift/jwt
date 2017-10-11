35c7c841618a75b98b4292743d14aec1afd991be
Cheikh Seck <Cheikh@OrendaCapitals-Mac-mini.local>
2017-10-11 13:57:07 +0000

    init

---
+++ jwt.go
@@ -0,0 +1,102 @@
+//go-jwt Cheikh Seck <cseck@orkiv.com>
+package jwt
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"gopkg.in/mgo.v2/bson"
+	"net/http"
+	"strings"
+)
+
+var Key = "RandomCode"
+
+type JSON bson.M
+
+const Sha512Header = `{"typ":"JWT","alg":"HS256"}`
+
+type Token struct {
+	Header, Payload bson.M
+}
+
+func toJWT(payload JSON) string {
+	message := fmt.Sprintf("%s.%s", Base64(Sha512Header), Base64(tojson(payload)))
+	return fmt.Sprintf("%s.%s", message, GenerateMAC([]byte(message)))
+}
+
+func Process(jwtstring string) (token Token, err error) {
+
+	bits := strings.Split(jwtstring, ".")
+
+	if len(bits) < 3 {
+		err = errors.New("Invalid JWT token!")
+		return
+	}
+	//...
+	uDec, _ := base64.URLEncoding.DecodeString(bits[2])
+
+	if !CheckMAC([]byte(fmt.Sprintf("%s.%s", bits[0], bits[1])), uDec) {
+		err = errors.New("Invalid signature!")
+		return
+	}
+
+	headerbase64, _ := base64.URLEncoding.DecodeString(bits[0])
+	payloadbase64, _ := base64.URLEncoding.DecodeString(bits[1])
+
+	err = json.Unmarshal(headerbase64, &token.Header)
+	if err != nil {
+		err = errors.New("Invalid header!")
+		return
+	}
+
+	err = json.Unmarshal(payloadbase64, &token.Payload)
+	if err != nil {
+		err = errors.New("Invalid Payload!")
+		return
+	}
+
+	return
+}
+
+func JWTFromRequest(r *http.Request) (token Token, err error) {
+	/*if !strings.Contains(r.Header.Get("Authorization"), "Bearer") {
+		err = errors.New("Invalid token type.")
+		return
+	}*/
+	bits := strings.Split(r.Header.Get("Authorization"), " ")
+
+	token, err = Process(bits[len(bits)-1])
+
+	return
+}
+
+func CancelRequest(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusUnauthorized)
+}
+
+// https://golang.org/pkg/crypto/hmac/
+func CheckMAC(message, messageMAC []byte) bool {
+	mac := hmac.New(sha256.New, []byte(Key))
+	mac.Write(message)
+	expectedMAC := mac.Sum(nil)
+	return hmac.Equal(messageMAC, expectedMAC)
+}
+
+func Base64(str string) string {
+	return base64.URLEncoding.EncodeToString([]byte(str))
+}
+
+func GenerateMAC(message []byte) string {
+	mac := hmac.New(sha256.New, []byte(Key))
+	mac.Write(message)
+	expectedMAC := mac.Sum(nil)
+	return base64.URLEncoding.EncodeToString(expectedMAC)
+}
+func tojson(v interface{}) string {
+	data, _ := json.Marshal(&v)
+	return string(data)
+}
---
+++ readme.md
@@ -0,0 +1,52 @@
+# go-jwt
+
+Generate and validate JWT ( open standard RFC 7519 )  tokens with Go. The project is also compatible with [golangserver.com](http://golangserver.com).
+
+# Install
+
+	go get github.com/cheikhshift/jwt
+
+	...
+	import "github.com/cheikhshift/jwt"
+
+How to install on Go server
+
+	<import src="github.com/cheikhshift/jwt/gos.gxml" />
+
+### Package interfaces
+
+#### JSON
+
+	type JSON bson.M
+
+Notes : This is the equivalent of `map[string]interface{}`
+
+
+#### Token
+
+	type Token struct {
+		Header, Payload bson.M
+	}
+
+
+### Processing tokens
+
+	func Process(jwtstring string) (token Token, err error)
+
+The function `jwt.Process` will convert a JWT string into a [jwt.Token](#token).
+
+Get token directly if your token is in an Authorization header :
+
+	func JWTFromRequest(r *http.Request) (token Token, err error) 
+
+
+### Handle Unauthorized request
+Use the following function to set the unauthorized request code : 
+
+	func CancelRequest(w http.ResponseWriter) 
+
+
+### Create a new token
+
+	func toJWT(payload JSON) string
+
